917513c738c15a196514f2d644f4d33936b1f353
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2018-12-03 21:41:16 -0700

    Added images to cards

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ var obscureAnimals = [
         bioName: 'Ogcocephalus darwini',
         info: 'A fish of unusual morphology found around the Galapagos Islands and off Peru at depths of 10 to 249 ft. Red-lipped batfish are mainly known for their bright red lips. Batfish are not good swimmers; they use their highly adapted pectoral fins to "walk" on the ocean floor. When the batfish reaches maturity, its dorsal fin becomes a single spine-like projection (thought to function primarily as a lure for prey). Like other anglerfish, the red-lipped batfish has a structure on its head known as illicium. This structure is employed for attracting prey.',
         image: 'red-lipped.jpg',
-        handler: function() {
-            window.alert("woop")
-        }
+        
     },
     {
         animalName: 'Goblin Shark',
         bioName: 'Mitsukurina owstoni',
         info: 'A rare species of deep-sea shark. Sometimes called a "living fossil", it is the only extant representative of the family Mitsukurinidae, a lineage some 125 million years old. This pink-skinned animal has a distinctive profile with an elongated, flattened snout, and highly protrusible jaws containing prominent nail-like teeth. It is usually between 10 and 13 ft long when mature, though it can grow considerably larger. Goblin sharks inhabit upper continental slopes, submarine canyons, and seamounts throughout the world at depths greater than 330 ft, with adults found deeper than juveniles. Various anatomical features of the goblin shark, such as its flabby body and small fins, suggest that it is sluggish in nature. This species hunts for teleost fishes, cephalopods, and crustaceans both near the sea floor and in the middle of the water column. Its long snout is covered with ampullae of Lorenzini that enable it to sense minute electric fields produced by nearby prey, which it can snatch up by rapidly extending its jaws. Small numbers of goblin sharks are unintentionally caught by deepwater fisheries. The International Union for Conservation of Nature (IUCN) has assessed it as Least Concern, despite its rarity, citing its wide distribution and low incidence of capture.',
         image: 'goblin-shark.jpg',
-        handler: function() {
-            window.alert("doop")
-        }
+       
+    },
+    {
+        animalName: 'Peacock Spider',
+        bioName: 'Maratus jactatus',
+        info: 'A male of the peacock spider species, which is nicknamed Sparklemuffin, lifts its leg as part of a mating dance.',
+        image: 'peacock_spider.jpeg'
     },
     {
         animalName: 'Blue Dragon',
@@ -24,21 +26,39 @@ var obscureAnimals = [
         image: 'glaucus-atlanticus.jpg'
     },
     {
-        animalName: 'Blue Dragon',
-        bioName: 'Glaucus Atlanticus',
-        info: 'A species of small, blue sea slug. These sea slugs are pelagic: they float upside down by using the surface tension of the water to stay up, where they are carried along by the winds and ocean currents. Glaucus atlanticus is camouflaged: the blue side of their body faces upwards, blending in with the blue of the water. The silver/grey side of the sea slugs faces downwards, blending in with the silvery surface of the sea.',
+        animalName: 'Dementor Wasp',
+        bioName: 'Ampulex dementor',
+        info: 'Researchers named a newfound wasp Ampulex dementor, or "dementor wasp" for short: The name was inspired by Harry Potter\'s dementors, ghostlike creatures that suck away a person\'s happy thoughts (and soul, if they\'re feeling ravenous). The wasp eats cockroaches in an impressively scary way. It injects venom into the cockroach\'s belly, turning its prey into an immobile "passive zombie," the researchers told Live Science in May. But the venom doesn\'t actually kill it, meaning the cockroach gets eaten alive by the dementor wasp afterward.',
+        image: 'dementor_wasp.jpg'
+    },
+    {
+        animalName: 'Aye-Aye',
+        bioName: 'Daubentonia madagascariensis',
+        info: 'Aye-ayes can be found only on the island of Madagascar. These rare animals may not look like primates at first glance, but they are related to chimpanzees, apes, and humans.',
         image: 'glaucus-atlanticus.jpg'
     },
     {
-        animalName: 'Blue Dragon',
-        bioName: 'Glaucus Atlanticus',
-        info: 'A species of small, blue sea slug. These sea slugs are pelagic: they float upside down by using the surface tension of the water to stay up, where they are carried along by the winds and ocean currents. Glaucus atlanticus is camouflaged: the blue side of their body faces upwards, blending in with the blue of the water. The silver/grey side of the sea slugs faces downwards, blending in with the silvery surface of the sea.',
-        image: 'glaucus-atlanticus.jpg'
+        animalName: 'Maned Wolf',
+        bioName: 'Chrysocyon brachyurus',
+        info: 'The largest canid in South America, the maned wolf looks like a fox, is called a wolf, and is closely related to neither. Maned wolves primarily eat small animals, fruits and vegetables. Maned wolves mark their territory with powerful-smelling urine and feces on hillocks and termite mounds along their borders. They do not howl, but instead emit loud barks or roar barks to let their mate know where they are, and to warn other wolves to stay away.',
+        image: 'maned_wolf.jpg'
     },
     {
-        animalName: 'Blue Dragon',
-        bioName: 'Glaucus Atlanticus',
-        info: 'A species of small, blue sea slug. These sea slugs are pelagic: they float upside down by using the surface tension of the water to stay up, where they are carried along by the winds and ocean currents. Glaucus atlanticus is camouflaged: the blue side of their body faces upwards, blending in with the blue of the water. The silver/grey side of the sea slugs faces downwards, blending in with the silvery surface of the sea.',
+        animalName: 'Dumbo Octopus',
+        bioName: 'Grimpoteuthis spp',
+        info: 'The Dumbo Octopus is known as the deepest living of all octopus species. They live on the seafloor or hover just slightly above it at depths of depths of 3000 to 4000 m (9800 to 13000 ft). The similarity of the ear-like fins protruding from the top of the mantle of the Dumbo octopus to the ears of the Disney character, Dumbo, the Flying elephant, led to the common name, dumbo. There are about 17 species of Dumbo octopus that belong to a group called “umbrella octopus,” because they are able to float with an umbrella-like look to their mantle. While these species can “flush” color as do more shallow species, their mouth structure is different. It has a degenerated radula-type opening that allows it to swallow prey whole instead of having to tear and grind it. This feeding characteristic unique to the genus Grimpoteuthis.',
+        image: 'dumbo_octopus.jpg'
+    },
+    {
+        animalName: 'Gerenuk',
+        bioName: 'Litocranius walleri',
+        info: 'Gerenuks have never been an abundant species. At Tsavo National Park in Kenya, where L. walleri is protected and enjoys good habitat, researchers have concluded that L. walleri makes up less then .5% of the total biomass of hoofed mammals in the park. Because they do not form large populations and their food is of limited supply they exhibit strange social interactions. Males are solitary and very territorial, they only associate with females during the mating season or when they are young. Dominant males establish territories by marking trees and shrubs with their preorbital gland. Other dominant males will enter another\'s territory without any aggression or defensiveness being displayed but a young male without its own territory will be run off if it enters a dominant male\'s territory. The size of a males territory can range from 300 to 850 acres and can support several indivduals. Females form small bands of up to about 10 individuals, usualy related adults with young. These groups of females and young roam freely throughout male territories. Young males that have just been weaned often form bachelor herds that roam nomadically until they become mature enough to establish their own territories and breed.',
+        image: 'gerenuk.jpg'
+    },
+    {
+        animalName: 'Blob Fish',
+        bioName: 'Psychrolutes marcidus',
+        info: 'The blobfish inhabits the deep waters off the coasts of mainland Australia and Tasmania, as well as the waters of New Zealand. Blobfish are typically shorter than 30 cm. The flesh of the blobfish is primarily a gelatinous mass with a density slightly less than water; this allows the fish to float above the sea floor without expending energy on swimming. Its relative lack of muscle is not a disadvantage as it primarily swallows edible matter that floats in front of it such as deep-ocean crustaceans. Blobfish are often caught as bycatch in bottom trawling nets. The popular impression of the blobfish as bulbous and gelatinous is partially an artifact of the decompression damage done to specimens when they are brought to the surface from the extreme depths in which they live. In their natural environment, blobfish appear more typical of their superclass Osteichthyes (bony fish).',
         image: 'glaucus-atlanticus.jpg'
     }
 ]
@@ -61,17 +81,32 @@ for (var i = 0; i < obscureAnimals.length; i++) {
     var card = document.createElement("div")
     card.classList.add("card", "col-md-4")
     var animal = obscureAnimals[i]
-    //above = animal is the current object we're iterating over
-    var itemHTML = 
-    //string interpolation within backticks
-    `
-    <div class="card-body">
-    <h3 class="card-title text-white bg-dark">&nbsp;${animal.animalName}</h3>
-    <p class="card-text">Type: ${animal.bioName}</p>
-    <p class="card-text">${animal.info}</p>
-    `;
-    card.innerHTML = itemHTML;
-    card.onclick = animal.handler
+
+    var image = animal.image
+    var cardImage = document.createElement('img')
+    cardImage.classList.add('card-img-top')
+    cardImage.src = image
+    card.appendChild(cardImage)
+
+    var cardBody = document.createElement('div')
+    cardBody.classList.add('card-body')
+    card.appendChild(cardBody)
+
+    var cardTitle = document.createElement('h5')
+    cardTitle.classList.add('card-title')
+    cardTitle.innerText = animal.animalName
+    cardBody.appendChild(cardTitle)
+
+    var bio = document.createElement('p')
+    bio.classList.add('card-text')
+    bio.innerText = animal.bioName
+    cardBody.appendChild(bio)
+
+    var cardText = document.createElement('p')
+    cardText.classList.add('card-text')
+    cardText.innerText = animal.info
+    cardBody.appendChild(cardText)
+
     var targetRow = document.getElementById(rowTarget)
     console.log("appending animal card to row", rowTarget, animal.animalName)
     targetRow.appendChild(card)
